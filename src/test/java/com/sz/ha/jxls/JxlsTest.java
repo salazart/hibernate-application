afa6fb9cb42f61acb60ca889a742701bff29be49
salazart <s.o.w@i.ua>
2018-08-09 12:05:16 +0300

    Some experience with JXLS

--- src/test/java/com/sz/ha/jxls/JxlsTest.java
+++ src/test/java/com/sz/ha/jxls/JxlsTest.java
@@ -17,33 +17,40 @@ public class JxlsTest {
 //        List<City> sities = Arrays.asList(new City("Kyiv"), new City("Khmelnytskyi"), new City("Lviv"));
 
 //        List<User> users = Arrays.asList(
-////                new User(Long.valueOf(1), "Ivan1", "Ivonov1","City1"),
-////                new User(Long.valueOf(2), "Ivan2", "Ivonov2","City1"),
-////                new User(Long.valueOf(3), "Ivan3", "Ivonov3","City1"),
-////                new User(Long.valueOf(3), "Ivan3", "Ivonov3","City1"),
-////                new User(Long.valueOf(3), "Ivan3", "Ivonov3","City1"),
-////                new User(Long.valueOf(3), "Ivan3", "Ivonov3","City1"),
-////                new User(Long.valueOf(3), "Ivan3", "Ivonov3","City1"),
-////                new User(Long.valueOf(3), "Ivan3", "Ivonov3","City1"),
+//                new User(Long.valueOf(1), "Ivan1", "Ivonov1","City1"),
+//                new User(Long.valueOf(2), "Ivan2", "Ivonov2","City1"),
+//                new User(Long.valueOf(3), "Ivan3", "Ivonov3","City1"),
+//                new User(Long.valueOf(3), "Ivan3", "Ivonov3","City1"),
+//                new User(Long.valueOf(3), "Ivan3", "Ivonov3","City1"),
+//                new User(Long.valueOf(3), "Ivan3", "Ivonov3","City1"),
+//                new User(Long.valueOf(3), "Ivan3", "Ivonov3","City1"),
+//                new User(Long.valueOf(3), "Ivan3", "Ivonov3","City1"),
 //                new User(Long.valueOf(3), "Ivan3", "Ivonov4","City1"),
 //                new User(Long.valueOf(4), "Ivan4", "Ivonov4","City1"),
 //                new User(Long.valueOf(5), "Ivan5", "Ivonov5","City1"),
 //                new User(Long.valueOf(5), "Ivan5", "Ivonov5","City1"));
 
         List<Place> places = Arrays.asList(
-                new Place("счет1", "Ошибка инкассаторов", 1, 49000, "БФ1 (в раскрывающемся списке перечень счетов и сумм)"),
-                new Place("счет2", "Ошибка инкассаторов", 2, 50000, "БФ1 (в раскрывающемся списке перечень счетов и сумм)"),
-                new Place("счет1", "Ошибка кассовых работников при формировании кассет", 4, 30000, "БФ1 (в раскрывающемся списке перечень счетов и сумм)"),
-                new Place("счет2", "Ошибка кассовых работников при формировании кассет", 3, 20000, "БФ1 (в раскрывающемся списке перечень счетов и сумм)"),
-                new Place("счет1", "Ошибка инкассаторов2", 4, 10000, "РОО1")
+                new Place("счет1", "Ошибка инкассаторов", 1, 49000, "БФ1"),
+                new Place("счет2", "Ошибка инкассаторов", 2, 50000, "БФ1"),
+                new Place("счет1", "Ошибка инкассаторов2", 2, 12000, "БФ1"),
+                new Place("счет2", "Ошибка инкассаторов2", 2, 9000, "БФ1"),
+                new Place("счет1", "Ошибка кассовых работников при формировании кассет", 2, 30000, "БФ1"),
+                new Place("счет2", "Ошибка кассовых работников при формировании кассет", 3, 20000, "БФ1"));
+
+        List<Place> places2 = Arrays.asList(
+                new Place("счет1", "", 2, 10000, "РОО1"),
+                new Place("счет2", "", 1, 10000, "РОО1"),
+                new Place("счет1", "", 2, 10000, "РОО2"),
+                new Place("счет2", "", 3, 10000, "РОО2")
         );
 
         JxlsTest jxlsTest = new JxlsTest();
-        try(InputStream is = jxlsTest.getClass().getResourceAsStream("/template12.xls")) {
+        try(InputStream is = jxlsTest.getClass().getResourceAsStream("/template18.xls")) {
             try (OutputStream os = new FileOutputStream("object_collection_output.xls")) {
                 Context context = new Context();
                 context.putVar("places", places);
-//                context.putVar("user", new User("Alex", "Syd"));
+                context.putVar("places2", places2);
                 JxlsHelper.getInstance().processTemplate(is, os, context);
             } catch (IOException e) {
                 e.printStackTrace();
